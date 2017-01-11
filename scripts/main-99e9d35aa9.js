80e9c086126aba24e95abf0b5b3266d256d973bf
Amr <merodiro@gmail.com>
2017-01-11 20:22:03 +0200

    Update 2017-01-11T18:21:57.362Z

---
+++ scripts/main-99e9d35aa9.js
@@ -0,0 +1,1 @@
+"use strict";$(function(){$(".responsive_headline").fitText(1.2,{minFontSize:"25px",maxFontSize:"60px"}),$("html").niceScroll({cursorcolor:"#E94F37",cursorwidth:"10px",cursorborder:"1px solid #E94F37",cursorborderradius:100,zindex:9999}),$(window).on("load",function(){$(".spinner-wrapper").fadeOut(500)})});
