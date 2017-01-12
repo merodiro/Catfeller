7071b6ccad19dd940b9ab73901d251138437c339
Amr <merodiro@gmail.com>
2017-01-13 01:33:30 +0200

    adding animation on scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,3 @@
-let wow = new WOW({
-  boxClass: 'wow', // default
-  animateClass: 'animated', // default
-  offset: 50, // default
-  mobile: true, // default
-  live: true // default
-})
-wow.init();
 $(() => {
   'use strict';
   $('.responsive_headline').fitText(1.2, {
@@ -28,3 +20,29 @@ $(() => {
     $('.responsive_headline').addClass('animated rubberBand');
   });
 });
+
+// window.sr = ScrollReveal({ reset: true });
+// sr.reveal('.member', { duration: 1000, scale: .5 }, 150);
+// sr.reveal('.service', { duration: 1000, scale: .5 }, 150);
+
+window.sr = ScrollReveal({reset: true, distance: '20px', duration: 300});
+
+function revealNextInSequence (sequence) {
+  if (sequence.next < sequence.elements.length) {
+    sr.reveal(sequence.elements[sequence.next++], {
+      afterReveal: function () {
+        revealNextInSequence(sequence)
+      }
+    })
+  }
+}
+
+function startSequence (selector) {
+  var sequence = {
+    elements: document.querySelectorAll(selector),
+    next: 0
+  }
+  revealNextInSequence(sequence)
+}
+
+startSequence('.scroll')
