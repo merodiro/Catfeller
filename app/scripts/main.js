6eb76a64258f5e4158c722f1ebb3761904ea7de4
Amr <merodiro@gmail.com>
2017-01-13 14:08:19 +0200

    add some linters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,8 @@
 $(() => {
-  'use strict';
   $('.responsive_headline').fitText(1.2, {
     minFontSize: '25px',
     maxFontSize: '60px'
-  });
+  })
 
   $('html').niceScroll({
     cursorcolor: '#E94F37',
@@ -12,25 +11,21 @@ $(() => {
     cursorborderradius: 100,
     smoothscroll: true,
     zindex: 9999
-  });
+  })
 
-  //loading screen
-  $(window).on('load', function() {
+  // loading screen
+  $(window).on('load', () => {
     $('.spinner-wrapper').fadeOut(500)
-    $('.responsive_headline').addClass('animated rubberBand');
-  });
-});
+    $('.responsive_headline').addClass('animated rubberBand')
+  })
+})
 
-// window.sr = ScrollReveal({ reset: true });
-// sr.reveal('.member', { duration: 1000, scale: .5 }, 150);
-// sr.reveal('.service', { duration: 1000, scale: .5 }, 150);
-
-window.sr = ScrollReveal({reset: true, distance: '20px', duration: 300});
+window.sr = ScrollReveal({ reset: true, duration: 200 })
 
 function revealNextInSequence (sequence) {
   if (sequence.next < sequence.elements.length) {
     sr.reveal(sequence.elements[sequence.next++], {
-      afterReveal: function () {
+      afterReveal () {
         revealNextInSequence(sequence)
       }
     })
@@ -38,7 +33,7 @@ function revealNextInSequence (sequence) {
 }
 
 function startSequence (selector) {
-  var sequence = {
+  const sequence = {
     elements: document.querySelectorAll(selector),
     next: 0
   }
